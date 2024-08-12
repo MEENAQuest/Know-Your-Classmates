08354a9f77cac60a967bb1555ab01e44e272b420
MEENAQuest <135314500+MEENAQuest@users.noreply.github.com>
2024-08-12 20:10:20 +0530

    Update Myclassmates_1.py

--- Myclassmates_1.py
+++ Myclassmates_1.py
@@ -1,8 +1,16 @@
+'''
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+'''
+
 import csv
 batchSize = int(input("Enter Batch Size: "))
 with open('names.csv','w',newline="") as csvfile:
-    fieldnames=['Name','Age','Height']
-    writer=csv.DictWriter(csvfile,fieldnames=fieldnames)
+    field=['Name','Age','Height']
+    writer=csv.DictWriter(csvfile,fieldnames=field)
     writer.writeheader()
 
     for i in range(batchSize):
